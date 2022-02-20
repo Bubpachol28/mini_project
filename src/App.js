1edd37f3526ca0a3bc00f1f00be9b8b25cfab272
Bubpachol <tanter281143@gmail.com>
2022-02-20 18:33:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBox from './home/search_image';
+import Account from './home/Account';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchBox />} />
+        <Route path="/profile/:id" element={<Account />} />
+      </Routes>
+    </Router>
   );
 }
 
